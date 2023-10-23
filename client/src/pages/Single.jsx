a8424aa48af34f6b5bff62bf6f9bc52642286b39
inoru <inoru375@gmail.com>
2023-10-23 10:46:10 +0700

    single update warnini

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -11,12 +11,10 @@ import DOMPurify from "dompurify";
 
 const Single = () => {
   const [post, setPost] = useState({});
-
+  const [showWarning, setShowWarning] = useState(false); // State untuk menampilkan peringatan
   const location = useLocation();
   const navigate = useNavigate();
-
   const postId = location.pathname.split("/")[2];
-
   const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
@@ -31,49 +29,62 @@ const Single = () => {
     fetchData();
   }, [postId]);
 
-  const handleDelete = async ()=>{
+  const handleDelete = async () => {
     try {
       await axios.delete(`/posts/${postId}`);
-      navigate("/")
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  const getText = (html) =>{
-    const doc = new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent
-  }
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
 
   return (
     <div className="single">
-      <div className="content">
-        <img src={`http://localhost:8800/uploads/${post.img}`} alt="" />
-        <div className="user">
-          {post.userImg && <img
-            src={post.userImg}
-            alt=""
-          />}
-          <div className="info">
-            <span>{post.username}</span>
-            <p>Posted {moment(post.date).fromNow()}</p>
-          </div>
-          {currentUser.username === post.username && (
-            <div className="edit">
-              <Link to={`/write?edit=2`} state={post}>
-                <img src={Edit} alt="" />
-              </Link>
-              <img onClick={handleDelete} src={Delete} alt="" />
-            </div>
-          )}
+      {showWarning && (
+        <div className="not-logged-in-warning">
+          <p>Anda belum mempunyai akun. Silakan login atau daftar untuk melihat posting ini.</p>
         </div>
-        <h1>{post.title}</h1>
-        <p
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(post.desc),
-          }}
-        ></p>      </div>
-      <Menu cat={post.cat}/>
+      )}
+      {currentUser ? (
+        <div className="content">
+          <img src={`http://localhost:8800/uploads/${post.img}`} alt="" />
+          <div className="user">
+            {post.userImg && (
+              <img src={post.userImg} alt="" />
+            )}
+            <div className="info">
+              <span>{post.username}</span>
+              <p>Posted {moment(post.date).fromNow()}</p>
+            </div>
+            {currentUser.username === post.username && (
+              <div className="edit">
+                <Link to={`/write?edit=2`} state={post}>
+                  <img src={Edit} alt="" />
+                </Link>
+                <img onClick={handleDelete} src={Delete} alt="" />
+              </div>
+            )}
+          </div>
+          <h1>{post.title}</h1>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(post.desc),
+            }}
+          ></p>
+        </div>
+      ) : (
+        // Jika pengguna belum masuk atau mendaftar, tampilkan pesan peringatan
+        <button onClick={() => setShowWarning(true)}>
+          Anda tidak memiliki akses untuk
+          <br/>update dan hapus data
+        </button>
+      )}
+      <Menu cat={post.cat} />
     </div>
   );
 };
---
+++ client/src/pages/Warning.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Warning = () => {
+  return (
+    <div className="not-logged-in-warning">
+      <p>Anda belum mempunyai akun. Silakan login atau daftar untuk melihat posting ini.</p>
+    </div>
+  );
+};
+
+export default Warning;
